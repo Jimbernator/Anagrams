7617df49039f7530019532fbf9583a74c269191a
Jamie Bartholomew <jjrbart@gmail.com>
2023-09-05 20:42:11 +0100

    Top words

--- Anagram.py
+++ Anagram.py
@@ -1,11 +1,13 @@
 import re
 from itertools import permutations
 
-# Function to load the English words from a dictionary file
-def load_dictionary(dictionary_file):
+# Function to load the first 50,000 English words from a dictionary file
+def load_dictionary(dictionary_file, num_words=50000):
     dictionary = set()
     with open(dictionary_file, 'r', encoding='utf-8') as file:
-        for line in file:
+        for line_number, line in enumerate(file, start=1):
+            if line_number > num_words:
+                break
             word, _ = line.strip().split()  # Split the line into word and frequency
             dictionary.add(word.lower())
     return dictionary
@@ -31,7 +33,9 @@ if __name__ == "__main__":
     dictionary_file = "en-2012/en.txt"  # Replace with the path to your dictionary file
     input_string = input("Enter the input string: ")
 
-    dictionary = load_dictionary(dictionary_file)
+    # Load the first 50,000 words from the dictionary
+    dictionary = load_dictionary(dictionary_file, num_words=50000)
+
     print("Loaded Dictionary (First 5 entries):", list(dictionary)[:5])
 
     anagrams = generate_anagrams(input_string, dictionary)
